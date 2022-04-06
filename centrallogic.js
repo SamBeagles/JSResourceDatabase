062245cf9c3bef4b6e870987a2c3c86f74199052
SamBeagles <samuellbeagles@gmail.com>
2022-04-06 17:53:53 +0100

    Update centrallogic.js

--- centrallogic.js
+++ centrallogic.js
@@ -7,8 +7,8 @@ let pricing = rtg*price;
 let property = "Name : ";
 let pHB = document.getElementsByClassName("pHB");
 let pHL = document.getElementsByClassName("pHL");
-let priceHigh = false;
-
+let priceHighState = false;
+let priceLowState = false;
 
 //priceStatus is the upper and lower bounds of the cost of a material
 
@@ -18,8 +18,8 @@ const resource = {
             id: x,
             name: title,
             price: pricing,
-            priceHigh: false,
-            priceLow: false,
+            priceHigh: priceHighState,
+            priceLow: priceLowState,
             togglePriceHigh: function(priceStatus){
                 this.priceHigh = priceStatus;
             },
@@ -29,20 +29,45 @@ const resource = {
         };
 
         function high(){
-            resource.price++;
-            console.log(resource.price);
+            if(resource.price>=3){
+                resource.priceHigh = true;
+                resource.price++;
+                console.log(resource);
+            }
+            else if(resource.pricing<=-3){
+                resource.priceHigh = false;
+                resource.priceLow = true;
+                resource.price++;
+                console.log(resource)
+            }
+            else{
+                resource.priceHigh = false;
+                resource.priceLow = false;
+                resource.price++;
+                console.log(resource);
+            }
+            
         }
         
         function low(){
+            if(resource.price<=-3){
+                resource.priceHigh = false;
+                resource.priceLow = true;
+                resource.price--;
+                console.log(resource);
+            }
+            else  if(resource.price>=5){
+                resource.price--;
+                resource.priceHigh = true;
+                resource.priceLow = false;
+                console.log(resource);
+            }
+            else{
             resource.price--;
-            console.log(resource.price);
+            resource.priceHigh = false;
+            resource.priceLow = false;
+            console.log(resource);
+            }
         }
 
-       function PriceHigh(){
-           if(resource.price===3){
-           console.log(resource);
-       }
-       else {
-           console.log(resource.price);
-           }
-       }
+        console.log(resource);
